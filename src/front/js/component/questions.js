b05facc4a027493ae3cbdb7f24137a772ddb3917
NilsonMarenco <137912131+NilsonMarenco@users.noreply.github.com>
2024-01-09 13:17:38 +0000

    componentes Question y manifest

---
+++ src/front/js/component/questions.js
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+import "../styles/questions.css";
+
+const Questions = () =>{
+    return(<>
+    
+	<p>Questions</p>
+    
+    </>)
+}
+
+export default Questions;
