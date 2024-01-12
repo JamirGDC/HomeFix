ef4d775ce38991f74eb354ad00b8dfb23045f3d1
Hans Aparicio <hansaparicio@gmail.com>
2024-01-12 10:30:11 +0000

    Add Authetication in BE

--- src/api/routes.py
+++ src/api/routes.py
@@ -5,6 +5,7 @@ from flask import Flask, request, jsonify, url_for, Blueprint
 from api.models import db, User
 from api.utils import generate_sitemap, APIException
 from flask_cors import CORS
+from flask_jwt_extended import create_access_token, jwt_required, get_jwt_identity
 
 api = Blueprint('api', __name__)
 
@@ -20,3 +21,29 @@ def handle_hello():
     }
 
     return jsonify(response_body), 200
+
+@api.route("/token", methods=["POST", "GET"])
+def create_token():
+    email = request.json.get("email", None)
+    password = request.json.get("password", None)
+
+    # Query your database for username and password
+    user = User.query.filter_by(email=email, password=password).first()
+
+    if user is None:
+        # The user was not found on the database
+        return jsonify({"msg": "Bad email or password"}), 401
+    
+    # Create a new token with the user id inside
+    access_token = create_access_token(identity=user.id)
+    return jsonify({ "token": access_token, "user_id": user.id })
+
+@api.route("/protected", methods=["GET"])
+@jwt_required()
+def protected():
+    # Access the identity of the current user with get_jwt_identity
+    current_user_id = get_jwt_identity()
+    user = User.query.filter_by(id = current_user_id).first()
+    
+    return jsonify({"id": user.id, "username": user.email }), 200
+    # return jsonify({"id": "cualquier cosa" }), 200
