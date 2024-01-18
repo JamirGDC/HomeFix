37a416311e290fbc8832607641c20f3a21794a4a
NilsonMarenco <137912131+NilsonMarenco@users.noreply.github.com>
2024-01-18 23:34:21 +0000

    agregando algo de estilo

--- src/front/js/component/questions.js
+++ src/front/js/component/questions.js
@@ -28,7 +28,7 @@ const Questions = () => {
       <h2 className="text-2xl font-bold mb-4">Preguntas Frecuentes</h2>
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
         {questions.map((question, index) => (
-          <div key={index} className="mb-4">
+          <div key={index} className="border border-solid border-gray-300 p-1">
             <h3 className="text-lg font-semibold mb-2">{question.question}</h3>
             <p>{question.answer}</p>
           </div>
